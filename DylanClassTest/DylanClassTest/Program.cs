fa86f96b86298c0e11cc016c8dc28197511352bf
DylanDrumgoole <dylan.drumgoole@fenergo.com>
2023-06-13 11:48:09 +0100

    13/06

--- DylanClassTest/DylanClassTest/Program.cs
+++ DylanClassTest/DylanClassTest/Program.cs
@@ -1,7 +1,18 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        
-    }
-}
+﻿
+
+using DylanClassTest;
+
+Console.WriteLine("Creating an employee");
+Console.WriteLine("--------------------\n");
+
+Employee dylan = new Employee("Dylan", "Drumgoole", "dylan@gmail.com", new DateTime(1998, 3, 5), 25);
+
+dylan.DisplayEmployeeDetails();
+
+dylan.PerformWork();
+dylan.PerformWork();
+dylan.PerformWork(5);
+dylan.PerformWork();
+
+double receivedWageDylan = dylan.ReceiveWage(true);
+Console.WriteLine($"Wage paid (message from Program): {receivedWageDylan}");
