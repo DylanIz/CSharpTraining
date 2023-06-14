e3d238528afc323df6d0d98ccd89819374f9c1e0
DylanDrumgoole <dylan.drumgoole@fenergo.com>
2023-06-14 10:37:45 +0100

    Json converter

--- DylanClassTest/DylanClassTest/Employee.cs
+++ DylanClassTest/DylanClassTest/Employee.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace DylanClassTest
 {
@@ -15,54 +16,104 @@ namespace DylanClassTest
         public int numberOfHoursWorked;
         public double wage;
         public double hourlyRate;
+        public EmployeeType employeeType;
 
         public DateTime birthDay;
 
         const int minimalHoursWorkedUnit = 1;
 
-        public Employee(string first, string last, string em, DateTime bd) : this(first,last,em,bd,0)
-        {
-            
-        }
-
-        public Employee(string first, string last, string em, DateTime bd, double rate)
+        public Employee(string first, string last, string em, DateTime bd, double rate, EmployeeType empType)
         {
             firstName = first;
             lastName = last;
             email = em;
             birthDay = bd;
             hourlyRate = rate;
+            employeeType = empType;
+        }
+
+        public Employee(string first, string last, string em, DateTime bd) : this(first, last, em, bd, 0, EmployeeType.StoreManager)
+        {
         }
 
         public void PerformWork()
         {
-            //numberOfHoursWorked++;
             PerformWork(minimalHoursWorkedUnit);
-            //Console.WriteLine($"{firstName} {lastName} has worked for {numberOfHoursWorked} hours(s)!");
         }
 
         public void PerformWork(int numberOfHours)
         {
             numberOfHoursWorked += numberOfHours;
 
-            Console.WriteLine($"{firstName} {lastName} has worked for {numberOfHoursWorked} hours(s)!");
+            Console.WriteLine($"{firstName} {lastName} has worked for {numberOfHours} hour(s)!");
+        }
+
+        public int CalculateBonus(int bonus)
+        {
+
+            if (numberOfHoursWorked > 10)
+                bonus *= 2;
+
+            Console.WriteLine($"The employee got a bonus of {bonus}");
+            return bonus;
+        }
+
+        //public int CalculateBonusAndBonusTax(int bonus, ref int bonusTax)
+        //{
+
+        //    if (numberOfHoursWorked > 10)
+        //        bonus *= 2;
+
+        //    if (bonus >= 200)
+        //    {
+        //        bonusTax = bonus / 10;
+        //        bonus -= bonusTax;
+        //    }
+
+        //    Console.WriteLine($"The employee got a bonus of {bonus} and the tax on the bonus is {bonusTax}");
+        //    return bonus;
+        //}
+
+        public int CalculateBonusAndBonusTax(int bonus, out int bonusTax)
+        {
+            bonusTax = 0;
+            if (numberOfHoursWorked > 10)
+                bonus *= 2;
+
+            if (bonus >= 200)
+            {
+                bonusTax = bonus / 10;
+                bonus -= bonusTax;
+            }
+
+            Console.WriteLine($"The employee got a bonus of {bonus} and the tax on the bonus is {bonusTax}");
+            return bonus;
         }
 
 
         public double ReceiveWage(bool resetHours = true)
         {
-            wage = numberOfHoursWorked * hourlyRate;
+            if (employeeType == EmployeeType.Manager)
+            {
+                Console.WriteLine($"An extra was added to the wage since {firstName} is a manager!");
+                wage = numberOfHoursWorked * hourlyRate * 1.25;
+            }
+            else
+            {
+                wage = numberOfHoursWorked * hourlyRate;
+            }
 
             Console.WriteLine($"{firstName} {lastName} has received a wage of {wage} for {numberOfHoursWorked} hour(s) of work.");
 
             if (resetHours)
                 numberOfHoursWorked = 0;
+
             return wage;
         }
 
         public void DisplayEmployeeDetails()
         {
-            Console.WriteLine($"\nFirst name: \t{firstName}\nLast name: \t{lastName}\nEmail:\t\t{email}\nBirthday: \t{birthDay.ToShortDateString()}\n");
+            Console.WriteLine($"\nFirst name: \t{firstName}\nLast name: \t{lastName}\nEmail: \t\t{email}\nBirthday: \t{birthDay.ToShortDateString()}\n");
         }
     }
 }
--- DylanClassTest/DylanClassTest/Program.cs
+++ DylanClassTest/DylanClassTest/Program.cs
@@ -1,18 +1,63 @@
 ﻿
 
+using System.Text;
 using DylanClassTest;
+
+
 
 Console.WriteLine("Creating an employee");
 Console.WriteLine("--------------------\n");
 
 Employee dylan = new Employee("Dylan", "Drumgoole", "dylan@gmail.com", new DateTime(1998, 3, 5), 25);
 
-dylan.DisplayEmployeeDetails();
 
-dylan.PerformWork();
-dylan.PerformWork();
-dylan.PerformWork(5);
-dylan.PerformWork();
+string dylanAsJson = dylan.ConvertToJson();
+Console.WriteLine(dylanAsJson);
 
-double receivedWageDylan = dylan.ReceiveWage(true);
-Console.WriteLine($"Wage paid (message from Program): {receivedWageDylan}");
+
+//string name = "Bethany";
+//string anotherName = name;
+//name += " Smith";
+
+//Console.Write("Name: " + name);
+//Console.WriteLine("Another name: " + anotherName);
+
+//string indexes = string.Empty;
+
+//for (int i = 0; i < 2500; i++)
+//{
+//    indexes += i.ToString();
+//}
+
+//StringBuilder builder = new StringBuilder();
+
+//builder.Append("Last name: ");
+//builder.AppendLine(lastName);
+//builder.Append("First name: ");
+//builder.Append(firstName);
+//string result= builder.ToString();
+
+
+
+
+//dylan.PerformWork(55);
+
+//int minimumBonus = 100;
+//int receivedBonus = dylan.CalculateBonus(minimumBonus);
+
+//Console.WriteLine($"The minimum bonus is {minimumBonus} and {dylan.firstName} has received a bonus of {receivedBonus}");
+//dylan.DisplayEmployeeDetails();
+
+//dylan.PerformWork();
+//dylan.PerformWork();
+//dylan.PerformWork(5);
+//dylan.PerformWork();
+
+//int a = 42;
+//int aCopy = a;
+
+
+//Console.WriteLine($"{a},{aCopy}");
+
+//double receivedWageDylan = dylan.ReceiveWage(true);
+//Console.WriteLine($"Wage paid (message from Program): {receivedWageDylan}");
