2369ab614a626dfb8cb6f29ee9296d9422faf772
DylanDrumgoole <dylan.drumgoole@fenergo.com>
2023-07-24 14:05:46 +0100

    Add routing and controller

---
+++ CityInfo/CityInfo.Api/Controllers/CitiesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.Api.Controllers
+{
+    [ApiController]
+
+    public class CitiesController : Controller
+    {
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<object>
+                {
+                    new { id = 1, Name = "Dublin" },
+                    new { id = 2, Name = "Cork" }
+                });
+        }
+    }
+}
