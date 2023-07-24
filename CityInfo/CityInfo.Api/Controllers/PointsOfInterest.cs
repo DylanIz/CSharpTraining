d96176b67ad078feb381fe0528c35614d6c82b42
DylanDrumgoole <dylan.drumgoole@fenergo.com>
2023-07-24 16:10:46 +0100

    Add xml support

--- CityInfo/CityInfo.Api/Controllers/PointsOfInterest.cs
+++ CityInfo/CityInfo.Api/Controllers/PointsOfInterest.cs
@@ -11,7 +11,36 @@ namespace CityInfo.Api.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<PointsOfInterestDto>> GetPointsOfInterest(int cityId)
         {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
 
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(city.PointsOfInterest);
+        }
+
+        [HttpGet("{pointofinterestid}")]
+        public ActionResult<PointsOfInterestDto> GetPointOfInterest(
+            int cityId, int pointOfInterstId)
+        {
+            var city = CitiesDataStore.Current.Cities
+                .FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterest = city.PointsOfInterest
+                .FirstOrDefault(c => c.Id == pointOfInterstId);
+
+            if (pointOfInterest == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(city.PointsOfInterest);
         }
     }
 }
