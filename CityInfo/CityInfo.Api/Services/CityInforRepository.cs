277bea913b1fd71d79cf5767b0edb4177e086b16
DylanDrumgoole <dylan.drumgoole@fenergo.com>
2023-07-26 15:35:24 +0100

    City Repository

---
+++ CityInfo/CityInfo.Api/Services/CityInforRepository.cs
@@ -0,0 +1,36 @@
+﻿using CityInfo.Api.Entities;
+
+namespace CityInfo.Api.Services
+{
+    public class CityInforRepository : ICityInfoRepository
+    {
+        public Task<IEnumerable<City>> GetCitiesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<City?> GetCityAsync(
+            int cityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PointOfInterest?> GetPointOfInterestForCityAsync(
+            int cityId, 
+            int pointOfInterestId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<PointOfInterest>> GetPointsOfInterestForCityAsync(
+            int cityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<IEnumerable<City>> ICityInfoRepository.GetCitiesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
