bf4432d7fb192d1301239786e244d6758e81efc0
DylanDrumgoole <dylan.drumgoole@fenergo.com>
2023-08-30 16:29:25 +0100

    Config/Extensions

--- OauthCourse/Dylan.IDP/HostingExtensions.cs
+++ OauthCourse/Dylan.IDP/HostingExtensions.cs
@@ -1,3 +1,4 @@
+using Dylan.IDP;
 using Serilog;
 
 namespace OauthCourse;
@@ -16,7 +17,8 @@ internal static class HostingExtensions
             })
             .AddInMemoryIdentityResources(Config.IdentityResources)
             .AddInMemoryApiScopes(Config.ApiScopes)
-            .AddInMemoryClients(Config.Clients);
+            .AddInMemoryClients(Config.Clients)
+            .AddTestUsers(TestUsers.Users);
 
         return builder.Build();
     }
